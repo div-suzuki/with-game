ddf966ffd30c50b3de46f8358c91e3697c5b6664
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-05-11 17:00:19 +0900

    修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -21,21 +21,4 @@ class RoomsController < ApplicationController
     params.require(:room).permit(:tag_id, :member_id, :text)
   end
 
-  def new
-    @room = Room.new
-  end
-
-  def create
-    @room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def room_params
-    params.require(:room).permit(:tag_id, :member, :text)
-  end
 end
