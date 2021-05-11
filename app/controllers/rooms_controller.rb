d6c511af8539919812b508c8cdb6a07d14725d70
div-suzuki <shoichiro.suzuki@di-v.co.jp>
2021-05-11 16:31:08 +0900

    active_hashの導入

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,26 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.includes(:user).order("created_at DESC")
   end
+<<<<<<< Updated upstream
+=======
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:tag_id, :member_id, :text)
+  end
+>>>>>>> Stashed changes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2021_04_19_092907) do
+=======
+ActiveRecord::Schema.define(version: 2021_05_03_161938) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "member_id", null: false
+    t.string "text", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+>>>>>>> Stashed changes
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
